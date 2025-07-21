30949e387e675bbc3b5bd13c86529811f1179f40
Tom Boone <tbc@tomboone.com>
2025-07-21 15:51:12 -0400

    fix broken config.py

--- src/mlb_today/config.py
+++ src/mlb_today/config.py
@@ -1,6 +1,10 @@
 """ Configuration for mlb-today """
 import os
 
+STORAGE_CONNECTION_STRING = os.getenv("STORAGE_CONNECTION_STRING")
+BLOB_CONTAINER_NAME = os.getenv("BLOB_CONTAINER_NAME")
+EMAIL_BLOB_CONTAINER_NAME = os.getenv("EMAIL_BLOB_CONTAINER_NAME")
+
 SUBSCRIPTION_ID = os.getenv("SUBSCRIPTION_ID")
 TARGET_RESOURCE_GROUP_NAME = os.getenv("TARGET_RESOURCE_GROUP_NAME")
 TARGET_FUNCTION_APP_NAME = os.getenv("TARGET_FUNCTION_APP_NAME")
@@ -21,3 +25,35 @@ STATS_ENDPOINT = "https://www.fangraphs.com/api/leaders/major-league/data"
 
 LOG_DIRECTORY = os.getenv("LOG_DIRECTORY")
 LOG_LEVEL = os.getenv("LOG_LEVEL")
+
+
+def get_azure_app_info() -> dict[str, str]:
+    """
+
+    Returns:
+
+    """
+    # Fallback app values
+    subscription_id = os.getenv("SUBSCRIPTION_ID", None)
+    resource_group_name = os.getenv("RESOURCE_GROUP_NAME", None)
+    app_name = os.getenv("WEBSITE_SITE_NAME", os.getenv("APP_NAME", ""))
+
+    # Get Subscription ID and Resource Group from WEBSITE_OWNER_NAME
+    website_owner_name = os.environ.get('WEBSITE_OWNER_NAME')
+    if website_owner_name:
+        parts = website_owner_name.split('+')
+        if len(parts) > 0:
+            subscription_id = parts[0]
+        if len(parts) > 1:
+            resource_group_region_parts = parts[1].split('-')
+            if len(resource_group_region_parts) > 2 and \
+               resource_group_region_parts[-1] in ['eastus', 'westus', 'centralus', 'northeurope', 'westeurope', 'southeastasia', 'uksouth', 'canadacentral', 'brazilsouth', 'australiaeast', 'japaneast', 'southindia', 'uaenorth', 'southafricanorth', 'koreacentral', 'francecentral', 'germanywestcentral', 'norwayeast', 'switzerlandnorth', 'qatarcentral', 'swedencentral', 'israelcentral', 'polandcentral', 'italycentral', 'newzealandnorth']:
+                resource_group_name = "-".join(resource_group_region_parts[:-1])
+            else:
+                resource_group_name = parts[1] # Fallback if no clear region suffix
+
+    return {
+        "app_name": app_name,
+        "subscription_id": subscription_id,
+        "resource_group_name": resource_group_name
+    }
